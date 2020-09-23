5567a7b0b71a3ff48598530168ea8cdcb48fa98d
Tokir123 <61796580+Tokir123@users.noreply.github.com>
2020-09-23 13:49:37 +0200

    commit message

--- weighting.py
+++ weighting.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 os.chdir("..") #Moves up one level
 
-labels = np.array(Image.open('Unet3D/images/training/source/rotated/labels/7.png'))
+labels = np.array(Image.open('images/training/source/rotated/labels/7.png'))
 cv2.connectedComponents(labels)
 #Import Bild als image
 #
@@ -18,7 +18,7 @@ ret=ret-1
 def max_neighbours(image,pixel):
     class_indication=0
 
-    if  not(min(pixel)<1|pixel[0]>image.shape[0]-1|pixel[1]>image.shape[1]-1):
+    if not((min(pixel)<1)|(pixel[0]>image.shape[0]-1)|(pixel[1]>image.shape[1]-1)):
         class_indication=np.max(image[pixel[0]-1:pixel[0]+1,pixel[1]-1:pixel[1]+1])
 
     return class_indication
@@ -27,19 +27,20 @@ def max_neighbours(image,pixel):
 
 
 
-weighting_all=np.zeros(image.shape+(ret,))
+weighting_all=np.zeros(labels.shape+(ret,))
 
 for i in range(ret):
     weighting_all[...,i]=(mask==i)
 
-for iteration in range(30):
+for iteration in range(10):
     for i in range(ret):
-        for pixel in np.ndindex(weighting_all[...,i]).shape)
-            if (weighting_all[pixel,i]==0):
+        print(i)
+        for pixel in np.ndindex(weighting_all[...,i].shape):
+             if (weighting_all[pixel+(i,)]==0):
                 if(max_neighbours(weighting_all[...,i], pixel)>0):
-                    weighting_all[pixel, i] = max_neighbor(weighting_all[..., i], pixel)+1
+                    weighting_all[pixel+(i,)] = max_neighbours(weighting_all[..., i], pixel)+1
 
-weighting_all[weighting_all==0]=40
+weighting_all[weighting_all==0]=11
 weighting_all=weighting_all-1
 
 weighting=np.zeros(labels.shape)
